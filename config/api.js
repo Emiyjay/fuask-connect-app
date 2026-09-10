c52190766ae471b680ac52a0dbfee4b8f96680b5
EMMANUEL JOHN <emmanueljohn98878@gmail.com>
2026-09-10 07:55:22 +0100

    feat: handle expired sessions globally

--- config/api.js
+++ config/api.js
@@ -1,7 +1,23 @@
 import axios from 'axios'
+import { clearSession } from '../utils/session'
 
 const api = axios.create({
-  baseURL: 'https://fuask-connect-backend.onrender.com/api'
+  baseURL: process.env.EXPO_PUBLIC_API_URL || 'https://fuask-connect-backend.onrender.com/api',
+  timeout: 15000
 })
+
+api.interceptors.response.use(
+  response => response,
+  async error => {
+    const status = error.response?.status
+    const hasAuthHeader = Boolean(error.config?.headers?.Authorization)
+
+    if (status === 401 && hasAuthHeader) {
+      await clearSession()
+    }
+
+    return Promise.reject(error)
+  }
+)
 
 export default api
